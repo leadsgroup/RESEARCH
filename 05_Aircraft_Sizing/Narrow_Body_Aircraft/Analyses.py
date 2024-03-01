6c72e26720053cd2ab788dad2772a297a69b84cb
Matthew Clarke <mclarke2@stanford.edu>
2024-03-01 11:38:48 -0600

    optimization for narrow body

---
+++ 05_Aircraft_Sizing/Narrow_Body_Aircraft/Analyses.py
@@ -0,0 +1,70 @@
+# Analyses.py 
+
+# ----------------------------------------------------------------------        
+#   Imports
+# ----------------------------------------------------------------------    
+
+import RCAIDE 
+
+# ----------------------------------------------------------------------        
+#   Setup Analyses
+# ----------------------------------------------------------------------  
+
+def setup(configs):
+    """Set up analyses for each of the different configurations."""
+
+    analyses = RCAIDE.Analyses.Analysis.Container()
+
+    # Build a base analysis for each configuration. Here the base analysis is always used, but
+    # this can be modified if desired for other cases.
+    for tag,config in configs.items():
+        analysis = base_analysis(config)
+        if tag == 'cruise_spoilers':
+            # this is done since drag is not sufficient for the desired profile
+            analysis.aerodynamics.settings.spoiler_drag_increment = 0.005    
+        analyses[tag] = analysis
+
+    return analyses 
+
+# ----------------------------------------------------------------------        
+#   Define Base Analysis
+# ----------------------------------------------------------------------  
+
+def base_analysis(vehicle): 
+    # ------------------------------------------------------------------
+    #   Initialize the Analyses
+    # ------------------------------------------------------------------     
+    analyses = RCAIDE.Analyses.Vehicle()
+
+    # ------------------------------------------------------------------
+    #  Weights
+    weights = RCAIDE.Analyses.Weights.Weights_Transport()
+    weights.vehicle = vehicle
+    analyses.append(weights)
+
+    # ------------------------------------------------------------------
+    #  Aerodynamics Analysis
+    aerodynamics = RCAIDE.Analyses.Aerodynamics.Subsonic_VLM()
+    aerodynamics.geometry = vehicle
+    aerodynamics.settings.number_spanwise_vortices   = 25
+    aerodynamics.settings.number_chordwise_vortices  = 5   
+    analyses.append(aerodynamics)
+ 
+    # ------------------------------------------------------------------
+    #  Energy
+    energy = RCAIDE.Analyses.Energy.Energy()
+    energy.networks = vehicle.networks
+    analyses.append(energy)
+
+    # ------------------------------------------------------------------
+    #  Planet Analysis
+    planet = RCAIDE.Analyses.Planets.Planet()
+    analyses.append(planet)
+
+    # ------------------------------------------------------------------
+    #  Atmosphere Analysis
+    atmosphere = RCAIDE.Analyses.Atmospheric.US_Standard_1976()
+    atmosphere.features.planet = planet.features
+    analyses.append(atmosphere)   
+
+    return analyses
