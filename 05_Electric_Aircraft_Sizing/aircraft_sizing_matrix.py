8294e205caf7316de12e5f536f5748f35ea6cc3c
MGuidotti <matteog2@illinois.edu>
2024-03-01 00:38:38 -0600

    SMP code

--- 05_Electric_Aircraft_Sizing/aircraft_sizing_matrix.py
+++ 05_Electric_Aircraft_Sizing/aircraft_sizing_matrix.py
@@ -1,143 +1,23 @@
 # imports  
 import numpy as np           
-import matplotlib.pyplot as plt 
-
-# Conceptual Design: Identification of a general description of the aircraft. The global shape and configuration is determined 
-#                    based on the mission reuirements. 
-#                    
-#                    The characterization process is based on a small set of parameters:
-#                    - MTOW = Maximum Take Off Weight/Design Gross Weight
-#                    - P_b or T = Power (for Propeller driven aircraft) or Thrust (for Jet driven aircraft) -> W/P_b or T/W
-#                    - S = Wing reference area -> W/S
-#                    
-#                    The conceptual design process is based on Mission Specification and derives from Market studies, 
-#                    Manufacturing studies, Technological studies, Specific requirements
-#                    The Mission Specification includes:
-#                    - Mission Requirements
-#                    - Certification Framework
-#                    - Mission Profiles: Schematic description of the mission
-#                    - Payload Requirements
-#                    - Pressurization Requirements 
-#
-#                    The conceptual design phase Sizing to Performance looks at:
-#                    - Performance Requirements: 
-#                                  - Point performance: Stall Speed, Max speed, Economic cruise speed, manoeuvring speed, max RoC
-#                                  - Intergral performance: Range, Endurance, Time to climb
-#                                  - Field performance: TO distance, LND distance, Balance field length (BFL)
-           
+import matplotlib.pyplot as plt            
 
 def main():
-    #Weight_Estimation()
     Performance_Requirements()    
     return
-
-#def Weight_Estimation():
-
-#M_OE  = M_a + M_m + M_b + M_g
-
-# Need historical statistical regressions at subsystem level, for each component
-# or other applicable models, such as physics-based analytic ones
-# Models involve power output characteristics
-# all power and energy mission requirements must be considered in order to solve 
-# Eq. 1 simultaneously with the choice of an appropriate design point on the Sizing 
-# Matrix Plot (SMP, also known as performance matching plot). 
-# Design power loading W_mto/P_s and wing loading W_mto/S, being W_mto = M_mto*g 
-# the design gross weight, P_s the shaft brake-power and S the wing surface
-
-#M_b = largest between M_b_E = E_b*e_b and M_b_P = P_b*p_b
-
-#P_b = max power req in TO and initial climb
-
-#BP is sized to fulfil maximum mission power first (sizing to power), and then increased, if needed, to provide energy for the flight phases below the hybrid transition altitude (sizing to energy).
-
-#M_m = P_s*p_m and p_m from historical statistical regression
-
-#M_g = P_g*p_g, P_g selected by designer
-
-#M_f = E_f*e_f
-
-# M_a = M_OE - M_e (Mass of eng in conv. planes)
-
-# Power and energy requirements to be applied in the mass estimation are obtained through the analysis of the sizing mission profile
-
-#M_mto = M_p + M_OE + M_f
-
-
-# M_p represents payload mass, M_a non-propulsive 
-# airframe mass, M_m electric motor mass, M_b BP 
-# mass, M_g PGS mass, and M_f fuel mass.
-    
-#   Based on historical regressions for W_E (Empty) and Fuel Fractions method for W_F (Fuel)
-#   W_TO = Take-Off weight
-#   W_ZF = Zero Fuel Weight
-#   W_F = Fuel Weight
-#   W_EO = Empty Operative Weight
-#   W_L = Payload Weight -> From Mission Requirements
-#   W_TF = Trip Fuel Weight    
-#   W_RF = Reserve Fuel Weight -> From Certification Requirements    
-#   W_E = Empty Weight 
-#   W_C = Crew Weight -> From Mission Requirements
-#
-#   W_TO = W_ZF + W_F
-#         = (W_EO + W_L) + (W_TF + W_RF)
-#         = (W_E + W_C + W_L) + (W_TF + W_RF)
-#
-#   Unknowns: W_E and W_TF
-#
-#   w_E = W_E/MTOW = f_E(MTOW)
-#   w_F = W_F/MTOW = f_F(MTOW)
-#   So: (1 - w_E - w_F)*MTOW = W_C + W_L
-#
-#   f_E(MTOW) found from historical regression, using statistical distribution 
-#   Roskam: log_10(MTOW) = A + B*log_10(W_E) from diagram: log_10(W_E) vs log_10(MTOW)
-#   Raymer: w_E = D*MTOW**C from diagram: w_E vs log_10(MTOW)
-#
-#   f_F(MTOW) found from Fuel Fractions method
-#   w_F = W_F/MTOW = (MTOW - W_final)/MTOW = 1 - W_final/W_initial
-#   Each mission phase has an initial and final weight (ex. 1, 2)
-#   W_final/W_initial = (W_1/MTOW)*(W_2/W_1)*(W_3/W_2)*...*(W_N/W_(N-1))
-#   For short missions, use statistical approach: ex. Warmup and TO: W_i/W_(i-1) = 0.97
-#                                                     Climb: as cruise at C_Y
-#                                                     LND: 0.995 
-#   For long phases (Cruise, Loiter): Use Breguet Formulation
     
 def Performance_Requirements():
     
-#   Once the MTOW has been determined, we can determine the Project Point in the Sizing Matrix Plot
-#   - Performance Requirements: 
-#                 - Point performance: Stall Speed, Max speed, Economic cruise speed, manoeuvring speed, max RoC
-#                 - Intergral performance: Range, Endurance, Time to climb
-#                 - Field performance: TO distance, LND distance, Balance field length (BFL)
-#
-#   Point Performance:
-#   - V_stall:
-#   V_S = np.sqrt(2*W/(rho_0*S*C_L_max)) = f(W/S) <= V_S*
-#   So: W/S <= (V_S*)**2/(2/(rho_0*C_L_max)) = (W/S)*
-#
-#   - LND distance: 
-#   Use statistical approach
-#   S_LND = k*V_SO**2 = f(W/S) <= S_LND*
-#   So: W/S <= (W/S)*
-#   
-#   - TO distance: 
-#   Use statistical approach
-#   S_TO = k0*P_TO = f(W/S, W/P) <= S_TO*
-#
-#   - Maximum speed:
-#   From P_req = P_av we obtain an expression in V**4 and V with two meaningful solutions, we pick the highest one
-#   V_H =  f(W/S, W/P) >= V_H*
-#
-#   Climb Performance:
-#   - Steepest Climb: max gamma 
-#     sin(gamma) = (T-D)/W so -> V_SC = f(W/S, W/P)
-#   - Fastest Climb: max V_v 
-#     V_v = V*sin(gamma) -> V_FC = f(W/S, W/P)
-#   - Ceiling: ex. V_v = 0
-#   - Time to Climb: t = int(dh/V_v) -> t = f(W/S, W/P)
-#
-#   Turning flight:
-#   - Instant turn: As in stall, V_s_n = np.sqrt(n)*V_stal -> f(W/S)
-#   - Sustained turn -> f(W/S, W/P and n)
+    # -------------------------------------------------------------------------------------    
+    # user input variables 
+    # -------------------------------------------------------------------------------------
+    
+    #MTOW    = 
+    #V_stall = 
+    #C_L_max = 
+    #rho     = 
+    
+        
     
     # Lift coefficient
     cL_max = 1.7
@@ -219,20 +99,15 @@ def Performance_Requirements():
     sigma5 = (1 + lamb * h_5 / theta_ref0) ** (-(1 + g / (R * lamb)))
     rho5 = sigma5 * rho0   
 
-    # -------------------------------------------------------------------------------------    
-    # user input variables 
-    # -------------------------------------------------------------------------------------
-    
-    
-    
-    
-    
+
     
 
     # -------------------------------------------------------------------------------------
     # Sizing calculations
-    # -------------------------------------------------------------------------------------    
-    # Stall speed
+    # ------------------------------------------------------------------------------------- 
+    
+    # Stall
+    
     v_s = 28  # kcas
     vs = v_s / 1.94384  # m/s
     
