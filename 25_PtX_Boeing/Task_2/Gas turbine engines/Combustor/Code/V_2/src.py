79a19d2f36405bb56304fa547dcad83c821fdf60
MGuidotti <matteog2@illinois.edu>
2024-09-23 09:26:10 -0500

    Code V2 update

--- 25_PtX_Boeing/Task_2/Gas turbine engines/Combustor/Code/V_2/src.py
+++ 25_PtX_Boeing/Task_2/Gas turbine engines/Combustor/Code/V_2/src.py
@@ -10,7 +10,8 @@ class PSR(object):
         self,
         TP: list,
         phi: float,
-        volume: float,
+        length: float,
+        area: float,        
         fuel_file: str,
         fuel_species: dict,
         oxidizer_species: dict,
@@ -21,8 +22,9 @@ class PSR(object):
         tot_mdot_fuel: float
     ) -> None:
         self.TP = list(TP)
-        self.phi = float(phi)
-        self.volume = float(volume)
+        self.phi = float(phi),        
+        self.length = float(length)
+        self.area = float(area)        
         self.fuel_file = str(fuel_file)
         self.fuel_species = dict(fuel_species)
         self.oxidizer_species = dict(oxidizer_species)
@@ -37,14 +39,14 @@ class PSR(object):
         self._set_mdot()
 
         self.reactor = ct.IdealGasReactor(self.fuel)
-        self.reactor.volume = self.volume
+        self.reactor.volume = self.length*self.area
 
         self.upstream = ct.Reservoir(self.fuel)
         self.inlet = ct.MassFlowController(self.upstream, self.reactor)
         self.inlet.mass_flow_rate = self.mdot
 
         self.rho = self.fuel.density
-        self.t_res = self.rho * self.volume / self.mdot
+        self.t_res = self.rho * self.reactor.volume / self.mdot
 
         self.reacnet = ct.ReactorNet([self.reactor])
 
@@ -85,11 +87,12 @@ class Combustor(object):
     
         self.psrs = []
         self.mixers = []
-        for i in range(nb_psr):
+        for i in range(combustor_input['N_PZ']):
             self.psrs.append(PSR(
-                TP=[T_stag_0, P_stag_0],
+                TP=[combustor_input['T_stag_0'], combustor_input['P_stag_0']],
                 phi=phi_PSR[i],
-                volume=V_PZ_PSR,
+                length=combustor_input['L_PZ'],
+                area=combustor_input['A_PZ'],
                 fuel_file=fuel_file,
                 fuel_species=dict_fuel,
                 oxidizer_species=dict_oxy,
@@ -149,3 +152,77 @@ class Combustor(object):
 #     nb_psr = 8
 #     if (nb_psr%2 == 1):
 #         raise ValueError("The number of PSR must be even.")
+
+def combustor(
+    high_fidelity_kin_mech,
+    dict_fuel,
+    dict_oxy,
+    T_stag_0, 
+    P_stag_0, 
+    FAR, 
+    FAR_TO,
+    FAR_st, 
+    m_dot_fuel, 
+    m_dot_fuel_TO, 
+    m_dot_air_id, 
+    m_dot_air_TO, 
+    N_PZ,
+    V_PZ, 
+    phi_PZ_des, 
+    S_PZ, 
+    phi_SZ_des_1, 
+    phi_SZ_des_2, 
+    phi_SZ_des_3,
+    F_SC, 
+    A_SZ, 
+    L_SZ
+    ):
+
+    # ------------------------------------------------------------------------------
+    # ----------------------------- Initial Parameters -----------------------------
+    # ------------------------------------------------------------------------------
+
+    f_air_PZ                = (m_dot_fuel_TO*F_SC)/(phi_PZ_des*m_dot_air_TO*FAR_st) # [-]       Fraction of total air present in the combustor that enters the Primary Zone
+    f_air_SZ                = 1 - f_air_PZ                                          # [-]       Fraction of total air present in the combustor that enters the Secondary Zone
+    m_dot_air_PZ            = f_air_PZ*m_dot_air_id                                 # [kg/s]    Air mass flow going through the Primary Zone
+    m_dot_air_SZ            = (f_air_SZ*m_dot_air_id)/3                             # [kg/s]    Air mass flow going through each dilution air inlet (3 inlets)
+    phi_sign                = ((m_dot_fuel*F_SC)/m_dot_air_PZ)/(FAR_st)             # [-]       Mean Equivalence Ratio
+    sigma_phi               = S_PZ*phi_sign                                         # [-]       Standard deviation of the Equivalence Ratio
+    m_dot_air_PSR           = m_dot_air_PZ                                          # [kg/s]    Air mass flow going through each PSR
+    m_dot_fuel_PSR          = m_dot_fuel                                            # [kg/s]    Fuel mass flow going through each PSR
+    V_PZ_PSR                = V_PZ/N_PZ                                             # [m**3]    Volume of each PSR
+    phi_PSR                 = np.linspace(0.001, 2*phi_sign, N_PZ)                  # [-]       Distribution of Equivalence Ratio through the PSRs
+    Delta_phi               = np.abs(phi_PSR[0] - phi_PSR[1])                       # [-]       Difference between two subsequent Equivalence Ratios
+    comp_fuel               = list(dict_fuel.keys())                                # [-]       Fuel components
+
+    if high_fidelity_kin_mech:
+        fuel_file              = ct.Solution('Jet_A_High_Fidelity.yaml')               # [-]       Import full fuel kinematic mechanism
+    else:
+        fuel_file              = ct.Solution('Jet_A_Low_Fidelity.yaml')                # [-]       Import surrogate fuel kinematic mechanism
+    
+
+    N_PZ = 8
+    if (N_PZ%2 == 1):
+        raise ValueError("The number of PSR must be even.")
+
+    psrs = []
+    mixers = []
+    for i in range(N_PZ):
+        psrs.append(PSR(
+            TP=[T_stag_0, P_stag_0],
+            phi=phi_PSR[i],
+            volume=V_PZ_PSR,
+            fuel_file=fuel_file,
+            fuel_species=dict_fuel,
+            oxidizer_species=dict_oxy,
+            mean_phi=phi_sign,
+            sigma_phi=sigma_phi,
+            delta_phi=Delta_phi,
+            tot_mdot_air=m_dot_air_PSR,
+            tot_mdot_fuel=m_dot_fuel_PSR
+        ))
+        if (i%2 == 1):
+            mixer = ct.IdealGasReactor(psrs[-2].fuel)
+            mixers.append(mixer)
+            psrs[-2].mix(mixer)
+            psrs[-1].mix(mixer)
