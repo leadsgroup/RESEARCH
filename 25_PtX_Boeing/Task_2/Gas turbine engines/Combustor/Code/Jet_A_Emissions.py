1b3b00c11e64170d81f0d17e2407897742f3546e
MGuidotti <matteog2@illinois.edu>
2024-09-24 12:05:30 -0500

    Update Jet_A_Emissions.py

--- 25_PtX_Boeing/Task_2/Gas turbine engines/Combustor/Code/Jet_A_Emissions.py
+++ 25_PtX_Boeing/Task_2/Gas turbine engines/Combustor/Code/Jet_A_Emissions.py
@@ -30,21 +30,24 @@ def main():
     FAR                     = 0.02                                                  # [-]       Fuel-to-Air ratio
     m_dot_air_tot           = 40                                                    # [kg/s]    Air mass flow going through all combustors
     
-    f_air_PZ                = combustor.f_air_PZ                                         # [-]       Fraction of total air present in the combustor that enters the Primary Zone         
-    N_comb                  = combustor.N_comb                                      # [-]       Number of can-annular combustors
-    N_PZ                    = combustor.N_PZ                                        # [-]       Number of PSR (EVEN, must match the number of PSR below)
-    A_PZ                    = combustor.A_PZ                                        # [m**2]    Primary Zone cross-sectional area     
-    L_PZ                    = combustor.L_PZ                                        # [m]       Primary Zone length  
-    N_SZ                    = combustor.N_SZ                                        # [-]       Number of dilution air inlets        
-    A_SZ                    = combustor.A_SZ                                        # [m**2]    Secondary Zone cross-sectional area
-    L_SZ                    = combustor.L_SZ                                        # [m]       Secondary Zone length  
-    phi_SZ                  = combustor.phi_SZ                                      # [-]       Equivalence Ratio for PFR   
+    f_air_PZ                = 0.20                                                  # [-]       Fraction of total air present in the combustor that enters the Primary Zone         
+    FAR_st                  = 0.068
+    N_comb                  = 10                                                    # [-]       Number of can-annular combustors
+    N_PZ                    = 8                                                     # [-]       Number of PSR (EVEN, must match the number of PSR below)
+    A_PZ                    = 0.15                                                  # [m**2]    Primary Zone cross-sectional area     
+    L_PZ                    = 0.0153                                                # [m]       Primary Zone length  
+    N_SZ                    = 3                                                     # [-]       Number of dilution air inlets        
+    A_SZ                    = 0.15                                                  # [m**2]    Secondary Zone cross-sectional area
+    L_SZ                    = 0.075                                                 # [m]       Secondary Zone length  
+    phi_SZ                  = 0.2                                                   # [-]       Equivalence Ratio for PFR    phi_PZ_des              = 0.6                                                   # [-]       Primary Zone Design Equivalence Ratio
+    S_PZ                    = 0.6                                                   # [-]       Mixing parameter, used to define the Equivalence Ratio standard deviation  
+    F_SC                    = 0.45                                                  # [-]       Fuel scaler
     
     m_dot_fuel_tot          = m_dot_air_tot*FAR                                     # [kg/s]    Fuel mass flow going through all combustors
     m_dot_air               = m_dot_air_tot/N_comb                                  # [kg/s]    Air mass flow inside each combustor, scaled inside each PSR to vary the Equivalence Ratio
     m_dot_fuel              = m_dot_fuel_tot/N_comb                                 # [kg/s]    Fuel mass flow inside each combustor                                                                                                                                                                    
-    phi_sign                = 0.7        # To be changed                                            # [-]       Primary Zone mean Equivalence Ratio
-    sigma_phi               = 0.5        # To be changed                            # [-]       Primary Zone Equivalence Ratio standard deviation                                                                                                                                                      
+    phi_sign                = (m_dot_fuel_tot*F_SC)/(m_dot_air_tot*f_air_PZ*FAR_st) # [-]       Primary Zone mean Equivalence Ratio
+    sigma_phi               = phi_sign*S_PZ                                         # [-]       Primary Zone Equivalence Ratio standard deviation                                                                                                                                                      
                                                                                     
     if high_fidelity_kin_mech:                                                               
         dict_fuel           = {'NC10H22':0.16449, 'NC12H26':0.34308,'NC16H34':0.10335, 'IC8H18':0.08630,'NC7H14':0.07945, 'C6H5C2H5': 0.07348,'C6H5C4H9': 0.05812, 'C10H7CH3': 0.10972} # [-]       Fuel species and corresponding mole fractions for full fuel model
@@ -63,9 +66,9 @@ def main():
                                                                                                                                                  
     gas, EI, T_stag_out, P_stag_out, h_stag_out = combustor(high_fidelity_kin_mech, dict_fuel, dict_oxy, T_stag_0, P_stag_0, FAR, m_dot_fuel, m_dot_air, N_PZ, A_PZ, L_PZ, phi_sign, phi_SZ, A_SZ, L_SZ, f_air_PZ, N_SZ, sigma_phi) # [-]       Run combustor function
                                                                                                 
-    sp_idx              = [gas.species_index(sp) for sp in list_sp]                 # [-]       Retrieve the species index
-    data_n              = [gas.T, T_stag_out, P_stag_out, h_stag_out] + list(gas.X[sp_idx]) + list(gas.Y[sp_idx]) + list(EI[sp_idx]) # [-]       Assign output variables  
-    df.loc[0]           = data_n                                                    # [-]       Assign output variables to df 
+    sp_idx                  = [gas.species_index(sp) for sp in list_sp]                 # [-]       Retrieve the species index
+    data_n                  = [gas.T, T_stag_out, P_stag_out, h_stag_out] + list(gas.X[sp_idx]) + list(gas.Y[sp_idx]) + list(EI[sp_idx]) # [-]       Assign output variables  
+    df.loc[0]               = data_n                                                    # [-]       Assign output variables to df 
                                                                                                 
     print('The Emission Index of CO2 is:', '%0.6f'%df.loc[0, 'EI_CO2'], 'kg_CO2/kg_fuel.') # [-]       Print the value of EI_CO2
     print('The Emission Index of CO is:', '%0.6f'%df.loc[0, 'EI_CO'], 'kg_CO/kg_fuel.') # [-]       Print the value of EI_CO
