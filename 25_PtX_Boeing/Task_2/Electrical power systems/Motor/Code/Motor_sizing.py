f16df6d28a63bb80b98fada59b5617c3babf99ac
MGuidotti <matteog2@illinois.edu>
2024-10-24 23:47:19 -0500

    Update Motor_sizing.py

--- 25_PtX_Boeing/Task_2/Electrical power systems/Motor/Code/Motor_sizing.py
+++ 25_PtX_Boeing/Task_2/Electrical power systems/Motor/Code/Motor_sizing.py
@@ -10,19 +10,28 @@ import numpy as np
 
 def main():
     
-    B_sign = 1  # average magnitude of the radial flux density produced by the rotor
-    A_sign = 1  # stator electrical loading
-    D      = 1  # stator inner diameter
-    L      = 1  # motor stack length
-    omega  = 1  # rotor angular velocity
+    B_sign = 1                                                  # average magnitude of the radial flux density produced by the rotor
+    D      = 1                                                  # stator inner diameter
+    k_w    = 0.95                                               # winding factor
+    I_tot  = 1                                                  # total current that passes through the stator in both axial directions
+    D      = 1                                                  # stator inner diameter
+    L      = 1                                                  # motor stack length
+    omega  = 1                                                  # rotor angular velocity
     
-    compute_basic_motor_sizing()
+    compute_basic_motor_sizing(B_sign, k_w, I_tot, D, L, omega)
     
     return
 
-def compute_basic_motor_sizing(B_sign, A_sign, D, L, omega):
+def compute_basic_motor_sizing(B_sign, k_w, I_tot, D, L, omega):
     
-    tau = (np.pi/2)*(B_sign*A_sign)*(D**2)*L    # torque
-    P   = omega*tau                             # motor power
+    A_sign = (k_w*I_tot)/(np.pi*D)                              # stator electrical loading    
+    tau    = (np.pi/2)*(B_sign*A_sign)*(D**2)*L                 # torque
+    P      = omega*tau                                          # motor power
+    B_g1   = 4*B_sign/np.pi                                     # peak magnetic flux density of the fundamental harmonic produced by the rotor
+    K_s1   = A_sign*np.pi/2                                     # peak fundamental value of the linear current density
+    P      = omega*(np.pi/4)*(B_g1*K_s1)*(D**2)*L               # motor power
     
     return
+
+if __name__ == '__main__': 
+    main()
