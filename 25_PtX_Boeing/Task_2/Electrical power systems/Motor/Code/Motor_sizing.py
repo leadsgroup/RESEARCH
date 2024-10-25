e0fab430458b26f1d899c939e8ef65075de14a5d
MGuidotti <matteog2@illinois.edu>
2024-10-24 23:59:06 -0500

    Update Motor_sizing.py

--- 25_PtX_Boeing/Task_2/Electrical power systems/Motor/Code/Motor_sizing.py
+++ 25_PtX_Boeing/Task_2/Electrical power systems/Motor/Code/Motor_sizing.py
@@ -24,12 +24,16 @@ def main():
 
 def compute_basic_motor_sizing(B_sign, k_w, I_tot, D, L, omega):
     
-    A_sign = (k_w*I_tot)/(np.pi*D)                              # stator electrical loading    
-    tau    = (np.pi/2)*(B_sign*A_sign)*(D**2)*L                 # torque
-    P      = omega*tau                                          # motor power
-    B_g1   = 4*B_sign/np.pi                                     # peak magnetic flux density of the fundamental harmonic produced by the rotor
-    K_s1   = A_sign*np.pi/2                                     # peak fundamental value of the linear current density
-    P      = omega*(np.pi/4)*(B_g1*K_s1)*(D**2)*L               # motor power
+    A_sign         = (k_w*I_tot)/(np.pi*D)                              # stator electrical loading    
+    tau            = (np.pi/2)*(B_sign*A_sign)*(D**2)*L                 # torque
+    P_version_1    = omega*tau                                          # motor power
+    B_g1           = 4*B_sign/np.pi                                     # peak magnetic flux density of the fundamental harmonic produced by the rotor
+    K_s1           = A_sign*np.pi/2                                     # peak fundamental value of the linear current density
+    P_version_2    = omega*(np.pi/4)*(B_g1*K_s1)*(D**2)*L               # motor power
+    torque_density = (1/2)*(B_sign*A_sign)*D                            # torque per unit volume
+    power_density  = omega*torque_density                               # power per unit rotor volume 
+    
+    #P_out + LOSS_speed = 
     
     return
 
