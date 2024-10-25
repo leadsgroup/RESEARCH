69ac8739d9dfd96d3e7ad1580195a68c65c581d7
MGuidotti <matteog2@illinois.edu>
2024-10-25 16:47:52 -0500

    Update Motor_sizing.py

--- 25_PtX_Boeing/Task_2/Electrical power systems/Motor/Code/Motor_sizing.py
+++ 25_PtX_Boeing/Task_2/Electrical power systems/Motor/Code/Motor_sizing.py
@@ -2,7 +2,9 @@
 #   Imports
 # ----------------------------------------------------------------------
 
-import numpy as np
+import numpy             as np
+from   scipy.integrate   import quad
+import matplotlib.pyplot as plt
 
 # ----------------------------------------------------------------------
 #   Main
@@ -10,13 +12,13 @@ import numpy as np
 
 def main():
     
-    B_sign = 1                                                  # average magnitude of the radial flux density produced by the rotor
-    D      = 1                                                  # stator inner diameter
-    k_w    = 0.95                                               # winding factor
-    I_tot  = 1                                                  # total current that passes through the stator in both axial directions
-    D      = 1                                                  # stator inner diameter
-    L      = 1                                                  # motor stack length
-    omega  = 1                                                  # rotor angular velocity
+    B_sign         = 1                                          # average magnitude of the radial flux density produced by the rotor
+    D              = 1                                          # stator inner diameter
+    k_w            = 0.95                                       # winding factor
+    I_tot          = 1                                          # total current that passes through the stator in both axial directions
+    D              = 1                                          # stator inner diameter
+    L              = 1                                          # motor stack length
+    omega          = 1                                          # rotor angular velocity
     
     compute_basic_motor_sizing(B_sign, k_w, I_tot, D, L, omega)
     
@@ -24,18 +26,144 @@ def main():
 
 def compute_basic_motor_sizing(B_sign, k_w, I_tot, D, L, omega):
     
-    A_sign         = (k_w*I_tot)/(np.pi*D)                              # stator electrical loading    
-    tau            = (np.pi/2)*(B_sign*A_sign)*(D**2)*L                 # torque
-    P_version_1    = omega*tau                                          # motor power
-    B_g1           = 4*B_sign/np.pi                                     # peak magnetic flux density of the fundamental harmonic produced by the rotor
-    K_s1           = A_sign*np.pi/2                                     # peak fundamental value of the linear current density
-    P_version_2    = omega*(np.pi/4)*(B_g1*K_s1)*(D**2)*L               # motor power
-    torque_density = (1/2)*(B_sign*A_sign)*D                            # torque per unit volume
-    power_density  = omega*torque_density                               # power per unit rotor volume 
+    # -----------------------------------------------------------------------------------------
+    # The Basic Motor Sizing Equation
+    # -----------------------------------------------------------------------------------------       
     
-    #P_out + LOSS_speed = 
+    A_sign         = (k_w*I_tot)/(np.pi*D)                      # stator electrical loading    
+    tau            = (np.pi/2)*(B_sign*A_sign)*(D**2)*L         # torque
+    P_version_1    = omega*tau                                  # motor power
+    B_g1           = 4*B_sign/np.pi                             # peak magnetic flux density of the fundamental harmonic produced by the rotor
+    K_s1           = A_sign*np.pi/2                             # peak fundamental value of the linear current density    
+    
+    # -----------------------------------------------------------------------------------------
+    # Power and Torque Density
+    # -----------------------------------------------------------------------------------------    
+    
+    P_version_2    = omega*(np.pi/4)*(B_g1*K_s1)*(D**2)*L       # motor power
+    torque_density = (1/2)*(B_sign*A_sign)*D                    # torque per unit volume
+    power_density  = omega*torque_density                       # power per unit rotor volume 
+    
+    # -----------------------------------------------------------------------------------------
+    # Efficiency and Power Factor
+    # -----------------------------------------------------------------------------------------     
+    
+    tau_out        = tau - (LOSS_speed/omega)                   # output torque
+    P_out          = omega*tau_out                              # output power
+
+    def f(i, v):
+        return i*v
+    
+    T              = 2            
+    P              = (1/T)*quad(f, 0, T)                        # real power
+    I              = 1                                          # amplitude of applied current
+    V              = 1                                          # amplitude of applied voltage
+    S              = I*V                                        # apparent power
+    PZ             = P/S                                        # power factor
+    Q              = np.sqrt(S**2 - P**2)                       # reactive power
+    
+    # -----------------------------------------------------------------------------------------
+    # Calculation of the Average Magnitude of the Radial Flux Density Produced by the Rotor, 𝑩
+    # -----------------------------------------------------------------------------------------
+    
+    # -----------------------------------------------------------------------------------------
+    # Magnetic Reluctance Networks
+    # -----------------------------------------------------------------------------------------
+    
+    MMF            = 1                                          # magnetomotive force applied to the reluctance path
+    R              = 1                                          # magnetic reluctance of the path
+    phi            = MMF/R                                      # magnetic flux through the reluctance path
+    B              = phi/A                                      # magnetic flux density
+    N              = 1                                          # number of turns
+    I              = 1                                          # current in each turn       
+    MMF_coil       = N*I                                        # MMF for a coil
+    Br             = 1                                          # remnant flux density
+    mu_0           = 1                                          # permeability of free space
+    mu_r           = 1                                          # relative permeability of the magnetic material
+    l_m            = 1                                          # thickness of the magnet or the size of the element if multiple elements span a magnet
+    MMF_magnet     = (Br/(mu_0*mu_r))*l_m                       # MMF for a magnet
+    l              = 1                                          # length of the path
+    A              = 1                                          # cross-sectional area of the reluctance path perpendicular to length 𝑙
+    R              = l/(A*mu_0*mu_r)                            # reluctance of a given path or given reluctant element
+    
+    # -----------------------------------------------------------------------------------------
+    # Simple Example of North South Array
+    # -----------------------------------------------------------------------------------------    
+    
+    l_g            = 1                                          # airgap size
+    A_m            = 1                                          # magnet area
+    R_ag           = l_g/(A_m*mu_0)                             # airgap reluctance
+    R_mag          = l_m/(A_m*mu_0*mu_r)                        # magnet reluctance
+    R              = 2*R_ag + 2*R_mag                           # reluctance of the path
+    MMF            = 2*(Br/(mu_0*mu_r))*l_m                     # magnetomotive force in the circuit
+    phi            = MMF/R                                      # flux in the path
+    B_m            = (Br*l_m)/(l_g*mu_r + l_m)                  # airgap field in the gap produced by the magnets
+    B_sign         = B_m                                        # for rotors where magnets span the full
+    alpha_m        = 1                                          # magnet’s pole span angle in electrical degrees
+    B_g1           = (4/np.pi)*B_m*np.sin(alpha_m/2)            # peak flux density magnitude of the fundamental harmonic, for rotors where the magnets do not span the full magnetic pole
+    
+    # -----------------------------------------------------------------------------------------
+    # Solution and plot
+    # -----------------------------------------------------------------------------------------      
+    
+    l_m            = np.linspace(0,0.02,100)                    # thickness of the magnet or the size of the element if multiple elements span a magnet
+    l_g            = 0.001                                      # airgap size
+    Br             = 1.2                                        # remnant flux density
+    mu_r           = 1.05                                       # relative permeability of the magnetic material
+    B_m            = (Br*l_m)/(l_g*mu_r + l_m)                  # airgap field in the gap produced by the magnets
+    
+    fig, ax1       = plt.subplots()
+    line1,         = ax1.plot(l_m, B_m, 'b-', label="B_m")
+    ax1.set_xlabel("Magnet Thickness [m]")
+    ax1.set_ylabel("B_m [T]", color='b')
+    ax1.tick_params(axis='y', labelcolor='b')
+    ax1.grid(True)
+    ax1.set_ylim(0, 1.4)
+    line2          = ax1.axhline(y=Br, color='r', linestyle='--', label="Br")
+                   
+    ax2            = ax1.twinx()
+    line3,         = ax2.plot(l_m, [bm / lm for bm, lm in zip(B_m, l_m)], 'k-', label="B_m/l_m")
+    ax2.set_ylabel("B_m / l_m [T/m]", color='k')
+    ax2.tick_params(axis='y', labelcolor='k')
+    ax2.set_ylim(0, 1200)
+    
+    lines          = [line1, line2, line3]
+    labels         = [line.get_label() for line in lines]
+    plt.legend(lines, labels, loc="upper left")
+    plt.title("Effect of Magnet Thickness on Gap Magnetic Field")
+    
+    # -----------------------------------------------------------------------------------------
+    # Stator Inductance Calculation
+    # -----------------------------------------------------------------------------------------   
+    
+    A_tip          = 1                                          # tooth tip area 
+    l_tip          = 1                                          # tooth tip gap width
+    W_sp           = 1                                          # stator pole width
+    R_tip          = l_tip/(A_tip*mu_0)                         # reluctance of the tip
+    R_rotor        = (2/3)*(l_m + l_g)/(W_sp*Stack*mu_0)        # reluctance of the rotor
+    R              = 1/((2/R_tip) + (1/R_rotor))                # reluctance of one coil
+    N              = 1                                          # number of turns
+    L_m            = N**2/R                                     # self-inductance of a single coil
+    
+    # -----------------------------------------------------------------------------------------
+    # Closed Form Field Solutions
+    # ----------------------------------------------------------------------------------------- 
+    
+    p              = 1                                          # pole count
+    Rm             = 1                                          # magnet outer radius
+    Rr             = 1                                          # magnet inner radius
+    Rs             = 1                                          # stator inner radius
+    
+    Br             = ((Br*(p/(p + 1))*(1 - (Rr/Rm)**(p + 1)))/(1 - ((Rr**(2*p))/(Rs))))*(((r/Rs)**(p - 1))*((Rm/Rs)**(p + 1)) + ((Rm/r)**(p + 1)))*np.cos(p*theta)
+    
+    r              = Rs
+    theta          = 0
+    Br_Rs_0        = ((Br*(p/(p + 1))*(1 - (Rr/Rm)**(p + 1)))/(1 - ((Rr**(2*p))/(Rs))))*(((r/Rs)**(p - 1))*((Rm/Rs)**(p + 1)) + ((Rm/r)**(p + 1)))*np.cos(p*theta)
+    B_sign         = (2/np.pi)*Br_Rs_0                          # average airgap field
+    
     
     return
 
 if __name__ == '__main__': 
     main()
+    plt.show()
