627c5feff6266e7367724f2d144a91247052dba3
Matthew Clarke <mclarke2@stanford.edu>
2024-11-08 13:02:55 -0600

    Updates to aeroacoustics

--- Aircraft/Tilt_Stopped_Rotor/Tilt_Stopped_Rotor.py
+++ Aircraft/Tilt_Stopped_Rotor/Tilt_Stopped_Rotor.py
@@ -9,7 +9,7 @@
 #   Imports
 # ---------------------------------------------------------------------
 import RCAIDE
-from RCAIDE.Framework.Core import Units 
+from RCAIDE.Framework.Core import Units
 from RCAIDE.Library.Methods.Geometry.Planform                                  import segment_properties,wing_segmented_planform    
 from RCAIDE.Library.Methods.Energy.Sources.Batteries.Common                    import initialize_from_circuit_configuration 
 from RCAIDE.Library.Methods.Weights.Correlation_Buildups.Propulsion            import compute_motor_weight
