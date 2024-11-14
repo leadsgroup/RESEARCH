0c32f5edf8998f15afe0422e0343e95af670bdca
MGuidotti <matteog2@illinois.edu>
2024-11-14 17:36:50 -0600

    Update Emissions_Parametric_Analysis.py

--- 25_PtX_Boeing/Task_2/Gas_turbine_engines/Combustor/2025_AIAA_Aviation/Emissions_Parametric_Analysis.py
+++ 25_PtX_Boeing/Task_2/Gas_turbine_engines/Combustor/2025_AIAA_Aviation/Emissions_Parametric_Analysis.py
@@ -20,66 +20,126 @@ import pickle
 
 def main():
     
- 
-    #parametric_analyses()
-    read_results('Emissions')
+    parametric_analyses()
+    #read_results('Emissions')
  
     return    
 
 def parametric_analyses():
-    comb_lengths      = np.linspace(0.08,  0.14,  5)
-    comb_areas        = np.linspace(0.15,  0.30,  5)
+    comb_lengths      = np.linspace(0.08,  0.1,   5)
+    comb_areas        = np.linspace(0.12,  0.18,  5)
     temperatures      = np.linspace(825,   875,   5)
     pressures         = np.linspace(2.8e6, 3.2e6, 5)
     mdots             = np.linspace(75,    85,    5) 
-    FARs              = np.linspace(0.015, 0.07,  5)
+    FARs              = np.linspace(0.015, 0.03,  5)
 
-    EI_CO2            = np.zeros((len(comb_lengths), len(comb_areas), len(temperatures), len(pressures), len(mdots), len(FARs)))
-    EI_CO             = np.zeros_like(EI_CO2)  
-    EI_H2O            = np.zeros_like(EI_CO2)  
-    #EI_NO             = np.zeros_like(EI_CO2)  
-    #EI_NO2            = np.zeros_like(EI_CO2) 
+    EI_CO2_L_A        = np.zeros((len(comb_lengths), len(comb_areas)))
+    EI_CO_L_A         = np.zeros_like(EI_CO2_L_A)  
+    EI_H2O_L_A        = np.zeros_like(EI_CO2_L_A)  
+    EI_CO2_T_P        = np.zeros((len(temperatures), len(pressures)))
+    EI_CO_T_P         = np.zeros_like(EI_CO2_T_P)  
+    EI_H2O_T_P        = np.zeros_like(EI_CO2_T_P)
+    EI_CO2_M_F        = np.zeros((len(mdots), len(FARs)))
+    EI_CO_M_F         = np.zeros_like(EI_CO2_M_F)  
+    EI_H2O_M_F        = np.zeros_like(EI_CO2_M_F)    
 
     combustor         = RCAIDE.Library.Components.Propulsors.Converters.Combustor()
     combustor.fuel_data = RCAIDE.Library.Attributes.Propellants.Jet_A1()  
 
-    #for f in fuel_types:
     for l in range(len(comb_lengths)):  
         for a in range(len(comb_areas)):   
-            for t in range(len(temperatures)):   
-                for p in range(len(pressures)): 
-                    for m in range(len(mdots)):  
-                        for far in range(len(FARs)):      
 
-                            combustor.A_PZ = comb_areas[a]         # [m**2]    Primary Zone cross-sectional area     
-                            combustor.L_PZ = 0.2*comb_lengths[l]   # [m]       Primary Zone length     
-                            combustor.A_SZ = comb_areas[a]         # [m**2]    Secondary Zone cross-sectional area
-                            combustor.L_SZ = 0.8*comb_lengths[l]   # [m]       Secondary Zone length          
-                            T              = temperatures[t]
-                            P              = pressures[p]
-                            mdot           = mdots[m]
-                            FAR            = FARs[far]
+            combustor.A_PZ = comb_areas[a]         # [m**2]    Primary Zone cross-sectional area     
+            combustor.L_PZ = 0.2*comb_lengths[l]   # [m]       Primary Zone length     
+            combustor.A_SZ = comb_areas[a]         # [m**2]    Secondary Zone cross-sectional area
+            combustor.L_SZ = 0.8*comb_lengths[l]   # [m]       Secondary Zone length          
+            T              = 850
+            P              = 3000000
+            mdot           = 80
+            FAR            = 0.0175
+            
+            print('Length =', "%0.3f" % comb_lengths[l] , "[m]")  
+            print('Area =', "%0.3f" % comb_areas[a] , "[m^2]")  
 
-                            results = evaluate_cantera(combustor,T,P,mdot,FAR)
+            results = evaluate_cantera(combustor,T,P,mdot,FAR)
 
-                            EI_CO2[l,a,t,p,m,far] = results.EI_CO2
-                            EI_CO[l,a,t,p,m,far]  = results.EI_CO 
-                            EI_H2O[l,a,t,p,m,far] = results.EI_H2O
-                            #EI_NO[l,a,t,p,m,far]  = results.EI_NO 
-                            #EI_NO2[l,a,t,p,m,far] = results.EI_NO2
-                            
-                            
-    #emission_indices  = [EI_CO2,EI_CO,EI_H2O,EI_NO,EI_NO2]  
-    emission_indices = {
-    "EI_CO2": EI_CO2,
-    "EI_CO": EI_CO,
-    "EI_H2O": EI_H2O}
+            EI_CO2_L_A[l,a] = results.EI_CO2
+            EI_CO_L_A[l,a]  = results.EI_CO 
+            EI_H2O_L_A[l,a] = results.EI_H2O
+            
+            emission_indices_L_A = {
+            "EI_CO2": EI_CO2_L_A,
+            "EI_CO": EI_CO_L_A,
+            "EI_H2O": EI_H2O_L_A}
+        
+            filename = '=emissions_L_A'
+            save_results(emission_indices_L_A, filename)
+      
 
-    filename = '=emissions'
-    save_results(emission_indices, filename)
+    for t in range(len(temperatures)):   
+        for p in range(len(pressures)):      
+
+            combustor.A_PZ = 0.15             # [m**2]    Primary Zone cross-sectional area     
+            combustor.L_PZ = 0.015            # [m]       Primary Zone length     
+            combustor.A_SZ = 0.15             # [m**2]    Secondary Zone cross-sectional area
+            combustor.L_SZ = 0.075            # [m]       Secondary Zone length          
+            T              = temperatures[t]
+            P              = pressures[p]
+            mdot           = 80
+            FAR            = 0.0175
+            
+            print('Temperature =', "%0.3f" % temperatures[t] , "[K]")  
+            print('Pressure =', "%0.3f" % pressures[p] , "[Pa]")             
+
+            results = evaluate_cantera(combustor,T,P,mdot,FAR)
+
+            EI_CO2_T_P[t,p] = results.EI_CO2
+            EI_CO_T_P[t,p]  = results.EI_CO 
+            EI_H2O_T_P[t,p] = results.EI_H2O
+            
+            emission_indices_T_P = {
+            "EI_CO2": EI_CO2_T_P,
+            "EI_CO": EI_CO_T_P,
+            "EI_H2O": EI_H2O_T_P}
+        
+            filename = '=emissions_T_P'
+            save_results(emission_indices_T_P, filename)
+    
+    
+    for m in range(len(mdots)):  
+        for far in range(len(FARs)):      
+    
+            combustor.A_PZ = 0.15    # [m**2]    Primary Zone cross-sectional area     
+            combustor.L_PZ = 0.015   # [m]       Primary Zone length     
+            combustor.A_SZ = 0.15    # [m**2]    Secondary Zone cross-sectional area
+            combustor.L_SZ = 0.075   # [m]       Secondary Zone length          
+            T              = 850
+            P              = 3000000
+            mdot           = mdots[m]
+            FAR            = FARs[far]
+            
+            print('Air mass flow rate =', "%0.3f" % mdots[m] , "[kg/s]")  
+            print('Fuel to Air Ratio =', "%0.3f" % FARs[far] , "[-]")            
+
+            results = evaluate_cantera(combustor,T,P,mdot,FAR)
+
+            EI_CO2_M_F[m,far] = results.EI_CO2
+            EI_CO_M_F[m,far]  = results.EI_CO 
+            EI_H2O_M_F[m,far] = results.EI_H2O
+            
+            emission_indices_M_F = {
+            "EI_CO2": EI_CO2_M_F,
+            "EI_CO": EI_CO_M_F,
+            "EI_H2O": EI_H2O_M_F}
+        
+            filename = '=emissions_M_F'
+            save_results(emission_indices_M_F, filename)
+
+    return  
 
 
-    return     
+
+
 def read_results(file_name):
     emissions = load_results(file_name)
     print(emissions)
