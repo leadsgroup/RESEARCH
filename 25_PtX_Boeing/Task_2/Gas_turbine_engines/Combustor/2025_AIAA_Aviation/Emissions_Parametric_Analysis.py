26263d844f53ea249d22ffc2ac437bd3c785328f
MGuidotti <matteog2@illinois.edu>
2024-11-14 16:34:39 -0600

    Update Emissions_Parametric_Analysis.py

--- 25_PtX_Boeing/Task_2/Gas_turbine_engines/Combustor/2025_AIAA_Aviation/Emissions_Parametric_Analysis.py
+++ 25_PtX_Boeing/Task_2/Gas_turbine_engines/Combustor/2025_AIAA_Aviation/Emissions_Parametric_Analysis.py
@@ -12,6 +12,7 @@ from   RCAIDE.Library.Plots                                                     
 import numpy             as np  
 import matplotlib.pyplot as plt  
 import os   
+import pickle
 
 # ----------------------------------------------------------------------
 #   Main
@@ -19,19 +20,26 @@ import os
 
 def main():
     
-    comb_lengths      = np.linspace(0.05, 0.14, 10)
-    comb_areas        = np.linspace(0.15, 0.30, 10)
-    temperatures      = np.linspace(700,  800,  10)
-    pressures         = np.linspace(20e6, 30e6, 10)
-    mdots             = np.linspace(1,    10,   10) 
-    FARs              = np.linspace(0.06, 0.07, 10)
-    
+ 
+    #parametric_analyses()
+    read_results('Emissions')
+ 
+    return    
+
+def parametric_analyses():
+    comb_lengths      = np.linspace(0.05, 0.14, 1)
+    comb_areas        = np.linspace(0.15, 0.30, 1)
+    temperatures      = np.linspace(700,  800,  1)
+    pressures         = np.linspace(20e6, 30e6, 1)
+    mdots             = np.linspace(1,    10,   1) 
+    FARs              = np.linspace(0.06, 0.07, 1)
+
     EI_CO2            = np.zeros((len(comb_lengths), len(comb_areas), len(temperatures), len(pressures), len(mdots), len(FARs)))
     EI_CO             = np.zeros_like(EI_CO2)  
     EI_H2O            = np.zeros_like(EI_CO2)  
-    EI_NO             = np.zeros_like(EI_CO2)  
-    EI_NO2            = np.zeros_like(EI_CO2) 
-    
+    #EI_NO             = np.zeros_like(EI_CO2)  
+    #EI_NO2            = np.zeros_like(EI_CO2) 
+
     combustor         = RCAIDE.Library.Components.Propulsors.Converters.Combustor()
     combustor.fuel_data = RCAIDE.Library.Attributes.Propellants.Jet_A1()  
 
@@ -51,18 +59,57 @@ def main():
                             P              = pressures[p]
                             mdot           = mdots[m]
                             FAR            = FARs[far]
-                            
+
                             results = evaluate_cantera(combustor,T,P,mdot,FAR)
-                            
+
                             EI_CO2[l,a,t,p,m,far] = results.EI_CO2
                             EI_CO[l,a,t,p,m,far]  = results.EI_CO 
                             EI_H2O[l,a,t,p,m,far] = results.EI_H2O
-                            EI_NO[l,a,t,p,m,far]  = results.EI_NO 
-                            EI_NO2[l,a,t,p,m,far] = results.EI_NO2
+                            #EI_NO[l,a,t,p,m,far]  = results.EI_NO 
+                            #EI_NO2[l,a,t,p,m,far] = results.EI_NO2
                             
                             
-    
-    return    
+    #emission_indices  = [EI_CO2,EI_CO,EI_H2O,EI_NO,EI_NO2]  
+    emission_indices = {
+    "EI_CO2": EI_CO2,
+    "EI_CO": EI_CO,
+    "EI_H2O": EI_H2O}
+
+    filename = '=emissions'
+    save_results(emission_indices, filename)
+
+
+    return     
+def read_results(file_name):
+    emissions = load_results(file_name)
+    print(emissions)
+    return  
+
+
+# ----------------------------------------------------------------------
+#   Save Results
+# ----------------------------------------------------------------------
+def save_results(results,filename):
+   #  Pickle Backup Files
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    load_dir = os.path.join(current_dir)
+    pickle_file = os.path.join(load_dir, filename + '.pkl')
+    with open(pickle_file, 'wb') as file:
+        pickle.dump(results, file) 
+    return
+
+# ------------------------------------------------------------------
+#   Load Results
+# ------------------------------------------------------------------   
+def load_results(filename):  
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    load_dir = os.path.join(current_dir)
+    load_file = os.path.join(load_dir, filename+ '.pkl')
+    with open(load_file, 'rb') as file:
+        results = pickle.load(file) 
+    return results
+
+
 
 if __name__ == '__main__': 
     main()
