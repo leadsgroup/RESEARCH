3e88684aec2d07fdde5c12181321628ce16c58c7
ARmolloy <167649645+ARmolloy@users.noreply.github.com>
2024-11-18 20:39:45 -0600

    Update HC_Operations_LA.py

--- 03_Aircraft_Noise_Modeling/City_Simulations/Los_Angeles/Hexacoper/HC_Operations_LA.py
+++ 03_Aircraft_Noise_Modeling/City_Simulations/Los_Angeles/Hexacoper/HC_Operations_LA.py
@@ -38,26 +38,6 @@ def main():
     flight_data     = pd.read_excel(routes_filepath,sheet_name=['Los_Angeles'])
     LA_flight_data         =  flight_data['Los_Angeles']
     
-    
-    operation_flight_times = np.array(['06:00:00','06:15:00','06:30:00','06:45:00',
-                                       '07:00:00','07:15:00','07:30:00','07:45:00',
-                                       '08:00:00','08:15:00','08:30:00','08:45:00',
-                                       '09:00:00','09:15:00','09:30:00','09:45:00',
-                                       '10:00:00','10:15:00','10:30:00','10:45:00',
-                                       '11:00:00','11:15:00','11:30:00','11:45:00', 
-                                       '12:00:00','12:15:00','12:30:00','12:45:00',
-                                       '13:00:00','13:15:00','13:30:00','13:45:00',
-                                       '14:00:00','14:15:00','14:30:00','14:45:00',
-                                       '15:00:00','15:15:00','15:30:00','15:45:00',
-                                       '16:00:00','16:15:00','16:30:00','16:45:00',
-                                       '17:00:00','17:15:00','17:30:00','17:45:00',
-                                       '18:00:00','18:15:00','18:30:00','18:45:00',
-                                       '19:00:00','19:15:00','19:30:00','19:45:00',
-                                       '20:00:00','20:15:00','20:30:00','20:45:00',
-                                       '21:00:00', ])
-    operation_period  = ['06:00:00','22:00:00']
-         
-
     mic_x_res                    = 1200
     mic_y_res                    = 1600 
     noise_timesteps              = 225  
@@ -72,7 +52,7 @@ def main():
     app_heading                  = 90  * Units.degree# Heading [degrees] of the approach to vertiport 2 
     max_cruise_distance          = 55*Units.nmi #CHANGE FOR EACH AIRCRAFT 
     
-    for i in len(LA_flight_data):
+    for i in range(len(LA_flight_data)):
         # Extract Data
         origin_code       = LA_flight_data['Origin Code'][i]   
         destination_code  = LA_flight_data['Destination Code'][i] 
