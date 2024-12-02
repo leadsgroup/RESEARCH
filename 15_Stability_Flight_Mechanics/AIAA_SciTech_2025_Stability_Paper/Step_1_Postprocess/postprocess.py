90ae227800cf934b8a30d0583fcb1ddc646e3d9c
ARmolloy <167649645+ARmolloy@users.noreply.github.com>
2024-12-02 14:31:14 -0600

    Update postprocess.py

--- 15_Stability_Flight_Mechanics/AIAA_SciTech_2025_Stability_Paper/Step_1_Postprocess/postprocess.py
+++ 15_Stability_Flight_Mechanics/AIAA_SciTech_2025_Stability_Paper/Step_1_Postprocess/postprocess.py
@@ -22,7 +22,7 @@ def main():
     #   Load Results
     # ------------------------------------------------------------------    
     
-    excel_file_name_1                   = "C:/Users/Matteo/Documents/UIUC/RESEARCH/15_Stability_Flight_Mechanics/AIAA_SciTech_2025_Stability_Paper/Step_1_Simulations/025_00_00_40_00_00_Baseline_stick_fixed_cruise_Opt_Results.xlsx"
+    excel_file_name_1                   = "/Users/aidanmolloy/Documents/LEADS/RESEARCH/15_Stability_Flight_Mechanics/AIAA_SciTech_2025_Stability_Paper/Step_1_Simulations/025_00_00_40_00_00_Baseline_stick_fixed_cruise_Opt_Results.xlsx"
     Baseline_Opt_Vehicle_1_pkl          = "025_00_00_40_00_00_Baseline_Opt_Vehicle"
     Optimized_Vehicle_1_pkl             = "025_00_00_40_00_00_Optimized_Vehicle"
     Output_Stick_Fixed_1_pkl            = "025_00_00_40_00_00_Output_Stick_Fixed"
@@ -31,7 +31,7 @@ def main():
     Optimized_Vehicle_1                 = load_results(Optimized_Vehicle_1_pkl            )
     Output_Stick_Fixed_1                = load_results(Output_Stick_Fixed_1_pkl           )
                                
-    excel_file_name_2                   = "C:/Users/Matteo/Documents/UIUC/RESEARCH/15_Stability_Flight_Mechanics/AIAA_SciTech_2025_Stability_Paper/Step_1_Simulations/025_00_00_41_00_00_Baseline_stick_fixed_cruise_Opt_Results.xlsx"
+    excel_file_name_2                   = "/Users/aidanmolloy/Documents/LEADS/RESEARCH/15_Stability_Flight_Mechanics/AIAA_SciTech_2025_Stability_Paper/Step_1_Simulations/025_00_00_41_00_00_Baseline_stick_fixed_cruise_Opt_Results.xlsx"
     Baseline_Opt_Vehicle_2_pkl          = "025_00_00_41_00_00_Baseline_Opt_Vehicle"
     Optimized_Vehicle_2_pkl             = "025_00_00_41_00_00_Optimized_Vehicle"
     Output_Stick_Fixed_2_pkl            = "025_00_00_41_00_00_Output_Stick_Fixed"
@@ -39,6 +39,9 @@ def main():
     excel_data_2                        = read_results(excel_file_name_2                  )
     Optimized_Vehicle_2                 = load_results(Optimized_Vehicle_2_pkl            )
     Output_Stick_Fixed_2                = load_results(Output_Stick_Fixed_2_pkl           )    
+    
+    
+    results = run_mission(Optimized_Vehicle_1)
     
     # ------------------------------------------------------------------
     #   Assign Variables 
@@ -149,7 +152,7 @@ def main():
 
 def load_results(filename):  
     # Define the directory where the file is located
-    current_dir = 'C:/Users/Matteo/Documents/UIUC/RESEARCH/15_Stability_Flight_Mechanics/AIAA_SciTech_2025_Stability_Paper/Step_1_Simulations'
+    current_dir = '/Users/aidanmolloy/Documents/LEADS/RESEARCH/15_Stability_Flight_Mechanics/AIAA_SciTech_2025_Stability_Paper/Step_1_Simulations'
     
     # Combine directory and filename to get full path
     load_file = os.path.join(current_dir, filename + '.pkl')
@@ -171,6 +174,164 @@ def read_results(file_name):
     data = excel_data.parse("Stick_Fixed")
     
     return data
+
+def run_mission(vehicle):
+    configs  = configs_setup(vehicle)
+
+    # vehicle analyses
+    analyses = analyses_setup(configs)
+
+    # mission analyses
+    mission  = mission_setup(analyses)
+    missions = missions_setup(mission) 
+
+    results = missions.base_mission.evaluate()
+    return(results)
+
+def analyses_setup(configs):
+
+    analyses = RCAIDE.Framework.Analyses.Analysis.Container()
+
+    # build a base analysis for each config
+    for tag,config in configs.items():
+        analysis = base_analysis(config)
+        analyses[tag] = analysis
+
+    return analyses
+
+def base_analysis(vehicle):
+
+    # ------------------------------------------------------------------
+    #   Initialize the Analyses
+    # ------------------------------------------------------------------     
+    analyses = RCAIDE.Framework.Analyses.Vehicle() 
+    
+    # ------------------------------------------------------------------
+    #  Weights
+    #weights         = RCAIDE.Framework.Analyses.Weights.Weights_EVTOL()
+    #weights.vehicle = vehicle
+    #analyses.append(weights)
+
+    # ------------------------------------------------------------------
+    #  Aerodynamics Analysis
+    aerodynamics         = RCAIDE.Framework.Analyses.Aerodynamics.Vortex_Lattice_Method() 
+    aerodynamics.vehicle = vehicle
+    aerodynamics.settings.drag_coefficient_increment = 0.0000
+    analyses.append(aerodynamics)
+
+
+    # ------------------------------------------------------------------
+    #  Stability Analysis
+    stability         = RCAIDE.Framework.Analyses.Stability.Vortex_Lattice_Method() 
+    stability.vehicle = vehicle
+    analyses.append(stability)    
+
+    # ------------------------------------------------------------------
+    #  Energy
+    #energy          = RCAIDE.Framework.Analyses.Energy.Energy()
+    #energy.vehicle = vehicle 
+    #analyses.append(energy)
+
+    # ------------------------------------------------------------------
+    #  Planet Analysis
+    planet = RCAIDE.Framework.Analyses.Planets.Planet()
+    analyses.append(planet)
+
+    # ------------------------------------------------------------------
+    #  Atmosphere Analysis
+    atmosphere = RCAIDE.Framework.Analyses.Atmospheric.US_Standard_1976()
+    atmosphere.features.planet = planet.features
+    analyses.append(atmosphere)   
+
+    # done!
+    return analyses    
+
+def configs_setup(vehicle):
+    '''
+    The configration set up below the scheduling of the nacelle angle and vehicle speed.
+    Since one prop_rotor operates at varying flight conditions, one must perscribe  the 
+    pitch command of the prop_rotor which us used in the variable pitch model in the analyses
+    Note: low pitch at take off & low speeds, high pitch at cruise
+    '''
+    # ------------------------------------------------------------------
+    #   Initialize Configurations
+    # ------------------------------------------------------------------ 
+    configs = RCAIDE.Library.Components.Configs.Config.Container() 
+    base_config                                                       = RCAIDE.Library.Components.Configs.Config(vehicle)
+    base_config.tag                                                   = 'base'     
+    configs.append(base_config) 
+ 
+    # ------------------------------------------------------------------
+    #   Cruise Configuration
+    # ------------------------------------------------------------------
+    config                                            = RCAIDE.Library.Components.Configs.Config(vehicle)
+    config.tag                                        = 'forward_flight'   
+    vector_angle                                      = 0.0 * Units.degrees   
+    config.networks.electric.busses.lift_rotor_bus.active  = False   
+    bus = config.networks.electric.busses.prop_rotor_bus  
+    for propulsor in  bus.propulsors:
+        propulsor.rotor.orientation_euler_angles =  [0, vector_angle, 0]
+        propulsor.rotor.pitch_command   = propulsor.rotor.cruise.design_pitch_command  
+    configs.append(config)     
+    
+    return configs
+ 
+
+# ----------------------------------------------------------------------
+#   Define the Mission
+# ---------------------------------------------------------------------- 
+def mission_setup(analyses): 
+    
+    # ------------------------------------------------------------------
+    #   Initialize the Mission
+    # ------------------------------------------------------------------
+
+    mission        = RCAIDE.Framework.Mission.Sequential_Segments()
+    mission.tag    = 'baseline_mission' 
+    
+    # unpack Segments module
+    Segments       = RCAIDE.Framework.Mission.Segments
+
+    # base segment           
+    base_segment   = Segments.Segment()
+    
+    #------------------------------------------------------------------------------------------------------------------------------------  
+    # Cruise 
+    #------------------------------------------------------------------------------------------------------------------------------------  
+    segment                                                          = Segments.Cruise.Constant_Speed_Constant_Altitude(base_segment)
+    segment.tag                                                      = "Cruise"  
+    segment.analyses.extend( analyses.forward_flight )                             
+    segment.altitude                                                 = 1000.0 * Units.ft  
+    segment.air_speed                                                = 110.  * Units['mph']  
+    segment.distance                                                 = 40 *Units.nmi 
+    segment.true_course                                              = 0 * Units.degree
+                                                                     
+    # define flight dynamics to model                                
+    segment.flight_dynamics.force_x                                  = True  
+    segment.flight_dynamics.force_z                                  = True     
+    
+    # define flight controls 
+    segment.assigned_control_variables.throttle.active               = True           
+    segment.assigned_control_variables.throttle.assigned_propulsors  = [['prop_rotor_propulsor_1','prop_rotor_propulsor_2','prop_rotor_propulsor_3',
+                                                                         'prop_rotor_propulsor_4','prop_rotor_propulsor_5','prop_rotor_propulsor_6'] ] 
+    segment.assigned_control_variables.body_angle.active             = True                
+         
+    mission.append_segment(segment)   
+   
+    return mission 
+
+
+def missions_setup(mission): 
+ 
+    missions         = RCAIDE.Framework.Mission.Missions()
+    
+    # base mission 
+    mission.tag  = 'base_mission'
+    missions.append(mission)
+ 
+    return missions 
+
+
 
 if __name__ == '__main__': 
     main()
