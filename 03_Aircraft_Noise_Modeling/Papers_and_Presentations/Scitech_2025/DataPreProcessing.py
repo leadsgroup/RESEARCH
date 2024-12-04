5f23e059a7322a1f2d201e0e89722b47cb0d3ef6
yair-13-g <yair13guerrero@gmail.com>
2024-12-03 18:45:27 -0600

    code fix finally thanks

---
+++ 03_Aircraft_Noise_Modeling/Papers_and_Presentations/Scitech_2025/DataPreProcessing.py
@@ -0,0 +1,162 @@
+import geopandas as gpd
+import pandas as pd
+from shapely.geometry import Point
+import shapely
+from rtree import index
+import numpy as np
+import os
+
+race_dictionary = {
+    "B03002001": "Total",
+    "B03002003": "White",
+    "B03002004": "Black",
+    "B03002005": "American Indian",
+    "B03002006": "Asian",
+    "B03002007": "Pacific Islander",
+    "B03002012": "Hispanic"
+}
+
+income_dictionary = {
+    "B19001001": "Total",
+    "B19001002": "<$10k",
+    "B19001003": "$10k to $15k",
+    "B19001004": "$15k to $20k",
+    "B19001005": "$20k to $25k",
+    "B19001006": "$25k to $30k",
+    "B19001007": "$30k to $35k",
+    "B19001008": "$35k to $40k",
+    "B19001009": "$40k to $45k",
+    "B19001010": "$45k to $50k",
+    "B19001011": "$50k to $60k",
+    "B19001012": "$60k to $75k",
+    "B19001013": "$75k to $100k",
+    "B19001014": "$100k to $125k",
+    "B19001015": "$125k to $150",
+    "B19001016": "$150k to $200k",
+    "B19001017": "$200k+ "
+}
+
+
+# def noise_preprocess(noise_file, census_tracts_file):
+#     # Load the existing census tracts GeoDataFrame
+#     census_tracts = census_tracts_file
+#     try:
+#         census_tracts = census_tracts.drop(['index_right'], axis=1)
+#     except:
+#         pass
+#     # Load noise data
+#     noise_data = noise_file
+
+#     # Extract noise columns (excluding Latitude and Longitude)
+#     noise_columns = noise_data.columns[2:]
+
+#     # Compute minimum noise values for each column
+#     threshold_values = {col: noise_data[col].min() for col in noise_columns}
+
+#     # Convert noise data to GeoDataFrame using vectorized approach
+#     noise_data['geometry'] = gpd.points_from_xy(
+#         noise_data['Longitude'] - 360, noise_data['Latitude']
+#     )
+#     noise_gdf = gpd.GeoDataFrame(noise_data, geometry='geometry', crs="EPSG:4326")
+
+#     # Perform spatial join to associate noise points with census tracts
+#     joined = gpd.sjoin(noise_gdf, census_tracts, how='inner', predicate='within')
+
+#     # Group by census tract and calculate mean noise levels
+#     mean_noise = joined.groupby('index_right')[noise_columns].mean().reset_index()
+
+#     # Merge the mean noise levels back into the census tracts GeoDataFrame
+#     census_tracts = census_tracts.merge(mean_noise, left_index=True, right_on='index_right', how='left')
+
+#     # Fill missing values with threshold values
+#     for col in noise_columns:
+#         census_tracts[col] = census_tracts[col].fillna(threshold_values[col])
+    
+#     return census_tracts
+
+
+def noise_preprocess(noise_file, census_tracts_file):
+    # Load the existing census tracts GeoDataFrame
+    census_tracts = census_tracts_file
+
+    # Load noise data
+    noise_data = noise_file
+
+    # Extract noise columns (excluding Latitude and Longitude)
+    noise_columns = noise_data.columns[2:]
+
+    # Compute minimum noise values for each column
+    threshold_values = {col: noise_data[col].min() for col in noise_columns}
+    
+    # Convert points to a GeoDataFrame
+    noise_data['geometry'] = noise_data.apply(
+        lambda row: Point(row['Longitude'] - 360, row['Latitude']), axis=1
+    )
+    noise_gdf = gpd.GeoDataFrame(noise_data, geometry='geometry', crs=census_tracts.crs)
+
+    # Perform spatial join to associate noise points with census tracts
+    joined = gpd.sjoin(noise_gdf, census_tracts, how='inner', predicate='within')
+
+    # Calculate mean noise levels for each census tract
+    mean_noise = joined.groupby('index_right')[noise_columns].mean()
+
+    # Merge the mean noise levels back into the census tracts GeoDataFrame
+    census_tracts = census_tracts.join(mean_noise)
+
+    # Fill missing values with threshold values
+    for col in noise_columns:
+        census_tracts[col] = census_tracts[col].fillna(threshold_values[col])
+
+    return census_tracts
+
+
+def sensitive_area_preprocess(file_list, census_tracts_file,file_names):
+    # Load the existing census tracts GeoDataFrame
+    demo_per_tract = census_tracts_file
+
+    try:
+        demo_per_tract = demo_per_tract.drop(['index_right'], axis=1)
+    except:
+        pass
+
+    i=0
+    for file in file_list:
+        loc_data = file
+        # Convert points to a GeoDataFrame
+        loc_data['geometry'] = loc_data.apply(
+            lambda row: Point(row['Longitude'], row['Latitude']), axis=1
+        )
+        loc_gdf = gpd.GeoDataFrame(loc_data, geometry='geometry', crs=demo_per_tract.crs)
+        
+        # Perform a spatial join to associate points with census tracts
+        joined = gpd.sjoin(loc_gdf, demo_per_tract, how='inner', predicate='within')
+
+        # Count points within each census tract
+        point_counts = joined.groupby('index_right').size()
+
+        # Add a new column for the point counts to the census tracts GeoDataFrame
+        demo_per_tract[file_names[i]] = demo_per_tract.index.map(point_counts).fillna(0).astype(int)
+        i+=1
+
+    return demo_per_tract
+
+
+def save_file(file,save_name):
+    file.to_file(save_name, driver="GeoJSON")
+
+def convert_csv(file,save_name):
+    df = file.copy()
+    df = df.drop(columns=['Latitude'])
+    df = df.drop(columns=['Longitude'])
+    df = df.drop(columns=['name_y'])
+    df = df.drop(columns=['geoid_y'])
+    df = df.loc[:, ~df.columns.str.contains('Error', case=False)]
+    df_proj = df.to_crs(epsg=3857)
+    # df['Area']  = (df_proj.geometry.area)*(3.861e-7)
+    df = df.drop(columns=['geometry'])
+
+    combined_dict = {**race_dictionary, **income_dictionary}
+    df = df.rename(columns=combined_dict)
+
+    df.to_csv(save_name, index=False)
+
---
+++ 03_Aircraft_Noise_Modeling/Papers_and_Presentations/Scitech_2025/UNIT.py
@@ -0,0 +1,107 @@
+from DataPreProcessing import*
+from NoiseImpactMapping import*
+from CommunityImpactMapping import*
+
+import geopandas as gpd
+import pandas as pd
+import os
+
+import time 
+
+race_dictionary = {
+    "Total": "B03002001",
+    "White": "B03002003",
+    "Black or African American": "B03002004",
+    "American Indian and Alaska Native": "B03002005",
+    "Asian": "B03002006",
+    "Native Hawaiian and Other Pacific Islander": "B03002007",
+    "Hispanic or Latino": "B03002012"
+}
+
+income_dictionary = {
+    "Total": "B19001001",
+    "Less than $10,000": "B19001002",
+    "$10,000 to $14,999": "B19001003",
+    "$15,000 to $19,999": "B19001004",
+    "$20,000 to $24,999": "B19001005",
+    "$25,000 to $29,999": "B19001006",
+    "$30,000 to $34,999": "B19001007",
+    "$35,000 to $39,999": "B19001008",
+    "$40,000 to $44,999": "B19001009",
+    "$45,000 to $49,999": "B19001010",
+    "$50,000 to $59,999": "B19001011",
+    "$60,000 to $74,999": "B19001012",
+    "$75,000 to $99,999": "B19001013",
+    "$100,000 to $124,999": "B19001014",
+    "$125,000 to $149,999": "B19001015",
+    "$150,000 to $199,999": "B19001016",
+    "$200,000 or More": "B19001017"
+}
+
+ti=time.time()
+
+# Define the separator for file paths
+separator = os.path.sep
+
+# Update the filenames for raw census data (race and income) in the Processed_Data directory
+base_geoJson = 'LA_Area_Tract.geojson'
+noise_filename = 'Noise_Data' + separator + 'Cumulative_HC_LA_1000ft.csv'
+struct_filename1 = 'Raw_Data' + separator + 'Churches.csv'
+struct_filename2 = 'Raw_Data' + separator + 'Schools_Colleges_and_Universities.csv'
+struct_filename3 = 'Raw_Data' + separator + 'Hospitals_and_Medical_Centers.csv'
+
+
+#load in files
+geoJson_file = gpd.read_file(base_geoJson)
+noise_file = pd.read_csv(noise_filename)
+struct_file1 = pd.read_csv(struct_filename1)
+struct_file2 = pd.read_csv(struct_filename2)
+struct_file3 = pd.read_csv(struct_filename3)
+
+
+struct_list=[struct_file1,struct_file2,struct_file3]
+
+
+#-------------------UNIT---------------#
+
+#Define Variables Based on Analysis
+sensitivity_levels = {
+"Churches": .5,
+"Schools": .83,
+"Hospitals/Medical Centers": .5
+}
+
+#Array of values you wish to analyze for noise or census
+census_columns=[race_dictionary['White'],race_dictionary['Black or African American'],race_dictionary['Hispanic or Latino'],
+               race_dictionary['Asian'], race_dictionary['Native Hawaiian and Other Pacific Islander']]
+noise_columns=['L_dn']
+
+#-------------Preprocessing Data---------#
+
+#Merging Data into one geoJSON
+# noise__struct_file = sensitive_area_preprocess(struct_list,geoJson_file,list(sensitivity_levels.keys()))
+# noise_census_file = noise_preprocess(noise_file,noise__struct_file)
+
+
+c_a_gdf = community_annoyance(geoJson_file,list(sensitivity_levels.keys()),sensitivity_levels)
+
+
+#Save merged data file (optional)
+# save_file(noise_census_file,'LA_Area_Tract.geojson')
+
+# convert_csv(noise_census_file,'HC_Data_Tract.csv')
+convert_csv(c_a_gdf,'HC_Data_Tract_CA.csv')
+
+
+
+#-----------------Plotting--------------#
+
+#Plotting function
+# impact_plotting(noise_census_file,noise_columns,min_lon= -119,min_lat=33,max_lon= -117.25,max_lat=35)
+# census_plotting(noise_census_file,census_columns,min_lon= -119,min_lat=33,max_lon= -117.25,max_lat=35)
+
+
+
+to = time.time()
+
+print((to-ti)/60)
